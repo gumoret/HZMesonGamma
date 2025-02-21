62b8f52c6116b824e81f6bda64e65d5effbe9d54
edoardo ferrando <edoardo.ferrando@edu.unito.it>
2025-02-21 11:28:30 +0100

    add GenLevel

---
+++ test/HZMesonGammaGenLevel.py
@@ -0,0 +1,224 @@
+import ROOT
+import argparse
+import math
+import numpy as np
+import sys
+from array import array
+from ROOT import TLorentzVector
+
+
+#Following bools are given as input
+verbose = True
+
+#Supress the opening of many Canvas's
+ROOT.gROOT.SetBatch(True)
+
+# PARSER and INPUT #############################################################################################
+p = argparse.ArgumentParser(description='Select rootfile to plot')
+p.add_argument('rootfile_name', help='Type rootfile name')
+p.add_argument('outputfile_option', help='Provide output file name')
+
+args = p.parse_args()
+fInput = ROOT.TFile(args.rootfile_name)
+output_filename = args.outputfile_option
+mytree = fInput.Get("Events")
+
+if not mytree:
+    print("Error: Tree 'Events' not found in file", args.rootfile_name)
+    sys.exit(1)
+
+
+#CREATE OUTPUT ROOTFILE ############################################################################################
+fOut = ROOT.TFile(output_filename,"RECREATE")
+fOut.cd()
+
+#Variables to go in the output tree ################################################################################
+_genBoson_ID    = np.zeros(1, dtype=int)
+_genBoson_pT    = np.zeros(1, dtype=float)
+_genBoson_eta   = np.zeros(1, dtype=float)
+_genBoson_phi   = np.zeros(1, dtype=float)
+_genBoson_mass  = np.zeros(1, dtype=float)
+
+_genMeson_ID    = np.zeros(1, dtype=int)
+_genMeson_pT    = np.zeros(1, dtype=float)
+_genMeson_eta   = np.zeros(1, dtype=float)
+_genMeson_phi   = np.zeros(1, dtype=float)
+_genMeson_mass  = np.zeros(1, dtype=float)
+
+_genGamma_ID    = np.zeros(1, dtype=int)
+_genGamma_pT    = np.zeros(1, dtype=float)
+_genGamma_eta   = np.zeros(1, dtype=float)
+_genGamma_phi   = np.zeros(1, dtype=float)
+
+_genTrack1_ID   = np.zeros(1, dtype=int)
+_genTrack1_pT   = np.zeros(1, dtype=float)
+_genTrack1_eta  = np.zeros(1, dtype=float)
+_genTrack1_phi  = np.zeros(1, dtype=float)
+_genTrack1_mass = np.zeros(1, dtype=float)
+
+_genTrack2_ID   = np.zeros(1, dtype=int)
+_genTrack2_pT   = np.zeros(1, dtype=float)
+_genTrack2_eta  = np.zeros(1, dtype=float)
+_genTrack2_phi  = np.zeros(1, dtype=float)
+_genTrack2_mass = np.zeros(1, dtype=float)
+
+
+tree_output = ROOT.TTree('tree_output','tree_output')
+tree_output.Branch('genBoson_ID', _genBoson_ID, '_genBoson_ID/I')
+tree_output.Branch('genBoson_pT', _genBoson_pT, '_genBoson_pT/D')
+tree_output.Branch('genBoson_eta', _genBoson_eta, '_genBoson_eta/D')
+tree_output.Branch('genBoson_phi', _genBoson_phi, '_genBoson_phi/D')
+tree_output.Branch('genBoson_mass', _genBoson_mass, '_genBoson_mass/D')
+
+tree_output.Branch('genMeson_ID', _genMeson_ID, '_genMeson_ID/I')
+tree_output.Branch('genMeson_pT', _genMeson_pT, '_genMeson_pT/D')
+tree_output.Branch('genMeson_eta', _genMeson_eta, '_genMeson_eta/D')
+tree_output.Branch('genMeson_phi', _genMeson_phi, '_genMeson_phi/D')
+tree_output.Branch('genMeson_mass', _genMeson_mass, '_genMeson_mass/D')
+
+tree_output.Branch('genGamma_ID', _genGamma_ID, '_genGamma_ID/I')
+tree_output.Branch('genGamma_pT', _genGamma_pT, '_genGamma_pT/D')
+tree_output.Branch('genGamma_eta', _genGamma_eta, '_genGamma_eta/D')
+tree_output.Branch('genGamma_phi', _genGamma_phi, '_genGamma_phi/D')
+
+tree_output.Branch('genTrack1_ID', _genTrack1_ID, '_genTrack1_ID/I')
+tree_output.Branch('genTrack1_pT', _genTrack1_pT, '_genTrack1_pT/D')
+tree_output.Branch('genTrack1_eta', _genTrack1_eta, '_genTrack1_eta/D')
+tree_output.Branch('genTrack1_phi', _genTrack1_phi, '_genTrack1_phi/D')
+tree_output.Branch('genTrack1_mass', _genTrack1_mass, '_genTrack1_mass/D')
+
+tree_output.Branch('genTrack2_ID', _genTrack2_ID, '_genTrack2_ID/I')
+tree_output.Branch('genTrack2_pT', _genTrack2_pT, '_genTrack2_pT/D')
+tree_output.Branch('genTrack2_eta', _genTrack2_eta, '_genTrack2_eta/D')
+tree_output.Branch('genTrack2_phi', _genTrack2_phi, '_genTrack2_phi/D')
+tree_output.Branch('genTrack2_mass', _genTrack2_mass, '_genTrack2_mass/D')
+
+#EVENTS LOOP #######################################################################################################
+nentries = mytree.GetEntriesFast()
+
+genBoson_ID, genBoson_pT, genBoson_eta, genBoson_phi, genBoson_mass = [-999] * 5
+genMeson_ID, genMeson_pT, genMeson_eta, genMeson_phi, genMeson_mass = [-999] * 5
+genGamma_ID, genGamma_pT, genGamma_eta, genGamma_phi = [-999] * 4
+genTrack1_ID, genTrack1_pT, genTrack1_eta, genTrack1_phi, genTrack1_mass = [-999] * 5
+genTrack2_ID, genTrack2_pT, genTrack2_eta, genTrack2_phi, genTrack2_mass = [-999] * 5
+
+
+for jentry in range(nentries):
+    ientry = mytree.LoadTree(jentry)
+    if ientry < 0: break
+    nb = mytree.GetEntry(jentry)
+    if nb <= 0: continue  
+    print("EVENT", jentry, " NUMBER OF GEN PARTICLES = ", mytree.nGenPart)        
+
+    # Create mother->daughters map for this event event
+    mother_to_daughters = {}
+    meson_to_daughters = {}  # meson map
+
+    for d in range(mytree.nGenPart):
+        mother_idx = mytree.GenPart_genPartIdxMother[d]
+        if 0 <= mother_idx < mytree.nGenPart:
+            if mother_idx not in mother_to_daughters: mother_to_daughters[mother_idx] = []
+            mother_to_daughters[mother_idx].append(d)
+
+            # Create mother->daughters map for mesons 
+            if mytree.GenPart_pdgId[mother_idx] in [333, 113, 313]:
+                if mother_idx not in meson_to_daughters: meson_to_daughters[mother_idx] = []
+                meson_to_daughters[mother_idx].append(d)  
+
+    for i in range(mytree.nGenPart):
+        daughters = mother_to_daughters.get(i, [])  # retrieve daughters
+
+        #if it is not a Higgs or a Z with 2 daughters (sometimes Pythia sends a Higgs in itself, therefore only 1 daughter), continue
+        if mytree.GenPart_pdgId[i] not in [23,25] or len(daughters) != 2: continue           
+            
+        meson_idx, gamma_idx = None, None
+
+        #for each daughter
+        for daughter in daughters:
+            if mytree.GenPart_pdgId[daughter] == 22: gamma_idx = daughter
+            elif mytree.GenPart_pdgId[daughter] in [333, 113, 313]: meson_idx = daughter
+
+        #if daughters are not Phi or Rho or K0* and gamma, continue
+        if meson_idx is None or gamma_idx is None: continue
+
+        #save boson variables
+        genBoson_ID = mytree.GenPart_pdgId[i]
+        genBoson_pT = mytree.GenPart_pt[i]
+        genBoson_eta = mytree.GenPart_eta[i]
+        genBoson_phi = mytree.GenPart_phi[i]
+        genBoson_mass = mytree.GenPart_mass[i]
+
+        #save Gamma variables
+        genGamma_ID = mytree.GenPart_pdgId[gamma_idx]
+        genGamma_pT = mytree.GenPart_pt[gamma_idx]
+        genGamma_eta = mytree.GenPart_eta[gamma_idx]
+        genGamma_phi = mytree.GenPart_phi[gamma_idx]
+
+        #save meson variables
+        genMeson_ID = mytree.GenPart_pdgId[meson_idx]
+        genMeson_pT = mytree.GenPart_pt[meson_idx]
+        genMeson_eta = mytree.GenPart_eta[meson_idx]
+        genMeson_phi = mytree.GenPart_phi[meson_idx]
+        genMeson_mass = mytree.GenPart_mass[meson_idx] 
+
+
+        meson_daughters = meson_to_daughters.get(meson_idx, [])#retrieve mesons daughters      
+
+        #if meson has not two daughters, continue
+        if len(meson_daughters) != 2: continue
+
+        #for each Meson daughter
+        for md in meson_daughters:
+            #if meson daughter is a K- or a Pi-
+            if mytree.GenPart_pdgId[md] in [-321, -211]:
+                genTrack1_ID = mytree.GenPart_pdgId[md]
+                genTrack1_pT = mytree.GenPart_pt[md]
+                genTrack1_eta = mytree.GenPart_eta[md]
+                genTrack1_phi = mytree.GenPart_phi[md]
+                genTrack1_mass = mytree.GenPart_mass[md]
+
+            #if meson daughter is a K+ or a Pi+
+            elif mytree.GenPart_pdgId[md] in [321, 211]:
+                genTrack2_ID = mytree.GenPart_pdgId[md]
+                genTrack2_pT = mytree.GenPart_pt[md]
+                genTrack2_eta = mytree.GenPart_eta[md]
+                genTrack2_phi = mytree.GenPart_phi[md]
+                genTrack2_mass = mytree.GenPart_mass[md]
+
+
+    _genBoson_ID[0] = genBoson_ID
+    _genBoson_pT[0] = genBoson_pT
+    _genBoson_eta[0] = genBoson_eta
+    _genBoson_phi[0] = genBoson_phi
+    _genBoson_mass[0] = genBoson_mass
+
+    _genMeson_ID[0] = genMeson_ID
+    _genMeson_pT[0] = genMeson_pT
+    _genMeson_eta[0] = genMeson_eta
+    _genMeson_phi[0] = genMeson_phi
+    _genMeson_mass[0] = genMeson_mass
+
+    _genGamma_ID[0] = genGamma_ID
+    _genGamma_pT[0] = genGamma_pT
+    _genGamma_eta[0] = genGamma_eta
+    _genGamma_phi[0] = genGamma_phi
+
+    _genTrack1_ID[0] = genTrack1_ID
+    _genTrack1_pT[0] = genTrack1_pT
+    _genTrack1_eta[0] = genTrack1_eta
+    _genTrack1_phi[0] = genTrack1_phi
+    _genTrack1_mass[0] = genTrack1_mass
+
+    _genTrack2_ID[0] = genTrack2_ID
+    _genTrack2_pT[0] = genTrack2_pT
+    _genTrack2_eta[0] = genTrack2_eta
+    _genTrack2_phi[0] = genTrack2_phi
+    _genTrack2_mass[0] = genTrack2_mass
+
+
+    tree_output.Fill()
+    
+#Tree writing ##########################################################################################################
+tree_output.Write()
+
+fOut.Close()
---
+++ test/generate_histos_GenLevel.py
@@ -0,0 +1,162 @@
+import ROOT
+import argparse
+import math
+import numpy as np
+import sys
+from array import array
+
+#Following bools are given as input
+isPhiAnalysis = False # for Z -> Phi Gamma
+isRhoAnalysis = False # for Z -> Rho Gamma
+isKAnalysis   = False # for Z -> K* Gamma
+isD0Analysis  = False # for Z -> D0* Gamma
+
+isHAnalysis   = False
+isZAnalysis   = False
+
+#Supress the opening of many Canvas's
+ROOT.gROOT.SetBatch(True)
+
+# PARSER and INPUT #############################################################################################
+p = argparse.ArgumentParser(description='Select rootfile to plot')
+p.add_argument('boson_channel', help='type <<H>> or <<Z>>')
+p.add_argument('meson_channel', help='type <<rho>> or <<phi>> or <<K*>> or <<D0*>>')
+p.add_argument('rootfile_name', help='Type rootfile name')
+p.add_argument('outputfile_option', help='Provide output file name')
+
+args = p.parse_args()
+fInput = ROOT.TFile(args.rootfile_name)
+output_filename = args.outputfile_option
+mytree = fInput.Get("tree_output")
+
+if args.boson_channel == "H": isHAnalysis = True 
+elif args.boson_channel == "Z": isZAnalysis = True 
+if args.meson_channel == "phi": isPhiAnalysis = True 
+if args.meson_channel == "rho": isRhoAnalysis = True 
+if args.meson_channel == "K*": isKAnalysis = True 
+if args.meson_channel == "D0*": isDAnalysis = True 
+
+
+#HISTOS ###########################################################################################################
+histo_map = dict()
+list_histos = ["h_bosonMass", "h_mesonMass", "h_firstTrkPt", "h_secondTrkPt", "h_firstTrkEta", "h_secondTrkEta", "h_firstTrkPhi", "h_secondTrkPhi", "h_mesonPt", "h_mesonEta", "h_mesonPhi", "h_photonPt", "h_photonEta", "h_photonPhi", "h_bosonPt", "h_bosonEta", "h_bosonPhi"] 
+
+histo_map[list_histos[0]]  = ROOT.TH1F(list_histos[0],"M_{boson}", 300, 50., 200.) 
+if   isPhiAnalysis: histo_map[list_histos[1]]  = ROOT.TH1F(list_histos[1],"M_{meson}", 100, 1., 1.05) 
+elif isRhoAnalysis: histo_map[list_histos[1]]  = ROOT.TH1F(list_histos[1],"M_{meson}", 100, 0.5, 1.) 
+histo_map[list_histos[2]]  = ROOT.TH1F(list_histos[2],"p_{T} of the 1st track", 100, 0.,70.)
+histo_map[list_histos[3]]  = ROOT.TH1F(list_histos[3],"p_{T} of the 2nd track", 100, 0., 50.)
+histo_map[list_histos[4]]  = ROOT.TH1F(list_histos[4],"#eta of the 1st track", 100, -2.5, 2.5)
+histo_map[list_histos[5]]  = ROOT.TH1F(list_histos[5],"#eta of the 2nd track", 100, -2.5, 2.5)
+histo_map[list_histos[6]]  = ROOT.TH1F(list_histos[6],"#phi of the 1st track", 100, -math.pi, math.pi)
+histo_map[list_histos[7]]  = ROOT.TH1F(list_histos[7],"#phi of the 2nd track", 100, -math.pi, math.pi)
+histo_map[list_histos[8]] = ROOT.TH1F(list_histos[8],"p_{T} of the meson", 100, 0., 140.)
+histo_map[list_histos[9]] = ROOT.TH1F(list_histos[9],"#eta_{meson}", 100, -2.5,2.5)
+histo_map[list_histos[10]] = ROOT.TH1F(list_histos[10],"#phi_{meson}", 100, -math.pi, math.pi)
+histo_map[list_histos[11]] = ROOT.TH1F(list_histos[11],"p_{T} of the #gamma", 100, 0., 140.)
+histo_map[list_histos[12]] = ROOT.TH1F(list_histos[12],"#eta_{#gamma}", 100, -2.5,2.5)
+histo_map[list_histos[13]] = ROOT.TH1F(list_histos[13],"#phi_{gamma}", 100, -math.pi, math.pi)
+histo_map[list_histos[14]] = ROOT.TH1F(list_histos[14],"p_{T} of boson", 100, 0., 250.)
+histo_map[list_histos[15]] = ROOT.TH1F(list_histos[15],"#eta_{boson}", 100, -2.5,2.5)
+histo_map[list_histos[16]] = ROOT.TH1F(list_histos[16],"#phi_{boson}", 100, -math.pi, math.pi)
+
+#CREATE OUTPUT ROOTFILE ############################################################################################
+fOut = ROOT.TFile(output_filename,"RECREATE")
+fOut.cd()
+
+#EVENTS LOOP ########################################################################################################
+nentries = mytree.GetEntriesFast()
+for jentry in range(nentries):
+    ientry = mytree.LoadTree( jentry )
+    if ientry < 0:
+        break
+    nb = mytree.GetEntry(jentry)
+    if nb <= 0:
+        print( "nb < 0")
+        continue
+
+    #Retrieve variables from the tree 
+    bosonMass      = mytree.genBoson_mass
+    mesonMass      = mytree.genMeson_mass
+    firstTrkPt     = mytree.genTrack1_pT      
+    secondTrkPt    = mytree.genTrack2_pT
+    firstTrkEta    = mytree.genTrack1_eta
+    secondTrkEta   = mytree.genTrack2_eta
+    firstTrkPhi    = mytree.genTrack1_phi
+    secondTrkPhi   = mytree.genTrack2_phi
+    mesonPt        = mytree.genMeson_pT      
+    mesonEta       = mytree.genMeson_eta
+    mesonPhi       = mytree.genMeson_phi
+    photonPt       = mytree.genGamma_pT
+    photonEta      = mytree.genGamma_eta
+    photonPhi      = mytree.genGamma_phi
+    bosonPt        = mytree.genBoson_pT
+    bosonEta       = mytree.genBoson_eta
+    bosonPhi       = mytree.genBoson_phi
+    
+
+    eventWeight = 1.
+    #FILL HISTOS #####################################################################################################
+    
+    histo_map["h_bosonMass"].Fill(bosonMass, eventWeight)          
+    histo_map["h_mesonMass"].Fill(mesonMass, eventWeight)
+    histo_map["h_firstTrkPt"].Fill(firstTrkPt, eventWeight)
+    histo_map["h_secondTrkPt"].Fill(secondTrkPt, eventWeight)
+    histo_map["h_firstTrkEta"].Fill(firstTrkEta, eventWeight)    
+    histo_map["h_secondTrkEta"].Fill(secondTrkEta, eventWeight)   
+    histo_map["h_firstTrkPhi"].Fill(firstTrkPhi, eventWeight)    
+    histo_map["h_secondTrkPhi"].Fill(secondTrkPhi, eventWeight)
+    histo_map["h_mesonPt"].Fill(mesonPt, eventWeight)
+    histo_map["h_mesonEta"].Fill(mesonEta, eventWeight)
+    histo_map["h_mesonPhi"].Fill(mesonPhi, eventWeight)
+    histo_map["h_photonPt"].Fill(photonPt, eventWeight)
+    histo_map["h_photonEta"].Fill(photonEta, eventWeight)
+    histo_map["h_photonPhi"].Fill(photonPhi, eventWeight)
+    histo_map["h_bosonPt"].Fill(bosonPt, eventWeight)
+    histo_map["h_bosonEta"].Fill(bosonEta, eventWeight)
+    histo_map["h_bosonPhi"].Fill(bosonPhi, eventWeight)
+        
+
+#HISTO LABELS #########################################################################################################
+histo_map["h_bosonMass"].GetXaxis().SetTitle("m_{Z} [GeV/c^2]")
+histo_map["h_bosonMass"].SetTitle("boson invariant mass")
+histo_map["h_mesonMass"].GetXaxis().SetTitle("m_{meson } [GeV/c^2]")
+histo_map["h_mesonMass"].SetTitle("Meson mass")
+histo_map["h_firstTrkPt"].GetXaxis().SetTitle("pT_{trk} [GeV/c]")
+histo_map["h_firstTrkPt"].SetTitle("Transverse momentum of the first charged particle ")
+histo_map["h_secondTrkPt"].GetXaxis().SetTitle("pT_{trk} [GeV/c]")
+histo_map["h_secondTrkPt"].SetTitle("Transverse momentum of the second charged particle")
+histo_map["h_firstTrkEta"].GetXaxis().SetTitle("#eta")
+histo_map["h_firstTrkEta"].SetTitle("Pseudorapidity of the first charged particle")
+histo_map["h_secondTrkEta"].GetXaxis().SetTitle("#eta")
+histo_map["h_secondTrkEta"].SetTitle("Pseudorapidity of the second charged particle")
+histo_map["h_firstTrkPhi"].GetXaxis().SetTitle("#phi [rad]")
+histo_map["h_firstTrkPhi"].SetTitle("Azimuthal angle of the first charged particle")
+histo_map["h_secondTrkPhi"].GetXaxis().SetTitle("#phi [rad]")
+histo_map["h_secondTrkPhi"].SetTitle("Azimuthal angle of the second charged particle")
+histo_map["h_mesonPt"].GetXaxis().SetTitle("pT_{meson} [GeV/c]") 
+histo_map["h_mesonPt"].SetTitle("Transverse momentum of the meson")
+histo_map["h_mesonEta"].GetXaxis().SetTitle("#eta")
+histo_map["h_mesonEta"].SetTitle("Pseudorapidity of the meson")
+histo_map["h_mesonPhi"].GetXaxis().SetTitle("#phi [rad]")
+histo_map["h_mesonPhi"].SetTitle("Azimuthal angle of the meson")
+histo_map["h_photonPt"].GetXaxis().SetTitle("p_{T}^{#gamma} [GeV/c]")
+histo_map["h_photonPt"].SetTitle("Transverse momentum of the photon")
+histo_map["h_photonEta"].GetXaxis().SetTitle("#eta_{#gamma}")
+histo_map["h_photonEta"].SetTitle("Eta of the photon")
+histo_map["h_photonPhi"].GetXaxis().SetTitle("#phi_{#gamma} [rad]")
+histo_map["h_photonPhi"].SetTitle("Phi of the photon")
+histo_map["h_bosonPt"].GetXaxis().SetTitle("pT_{boson} [GeV/c]")
+histo_map["h_bosonPt"].SetTitle("Transverse momentum of the boson")
+histo_map["h_bosonEta"].GetXaxis().SetTitle("#eta_{boson}")
+histo_map["h_bosonEta"].SetTitle("Eta of the Z")
+histo_map["h_bosonPhi"].GetXaxis().SetTitle("#phi_{boson} [rad]")
+histo_map["h_bosonPhi"].SetTitle("Phi of the boson")
+
+
+#HISTOS WRITING ########################################################################################################
+fOut.cd()
+for hist_name in list_histos:
+    histo_map[hist_name].Write()
+
+fOut.Close()    
