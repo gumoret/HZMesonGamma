2b44eac992b9cbb1c955e7e2c2da4193f98b44ed
Edoardo Ferrando <eferrand@submit02.mit.edu>
2026-03-19 11:14:55 -0400

    add d0 channel in rdf

--- test/RDF_analyzer.py
+++ test/RDF_analyzer.py
@@ -41,7 +41,7 @@ import glob
 #base_dir = "/ceph/submit/data/group/cms/store/user/mariadlf/D07/GJ-4Jets_Bin-HT-1000-PTG-100to200_Par-dRGJ-0p25_TuneCP5_13p6TeV_madgraphMLM-pythia8+RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v2+MINIAODSIM/"
 #files = sorted(glob.glob(f"{base_dir}/*.root")) 
 
-
+'''
 #Tau2022
 base_dir = "/scratch/submit/cms/mariadlf/Hrare/newSKIMS/D05" 
 
@@ -49,7 +49,7 @@ files = []
 
 for era in ["12022", "22022"]:
     files.extend(glob.glob(f"{base_dir}/{era}/Tau+Run*/*.root"))
-
+'''
 #EGamma 2024
 '''
 base_dir = "/scratch/submit/cms/mariadlf/Hrare/newSKIMS/D07/2024"
@@ -58,7 +58,7 @@ files = []
 files.extend(glob.glob(f"{base_dir}/EGamma*/*.root"))
 '''
 
-files = sorted(files)
+#files = sorted(files)
 
 #HRhogamma signal
 #base_dir = "/ceph/submit/data/group/cms/store/user/mariadlf/D07/GluGluHtoRhoG_Par-M-125_TuneCP5_13p6TeV_powheg-pythia8-evtgen+RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v2+MINIAODSIM"
@@ -69,6 +69,9 @@ files = sorted(files)
 #HKstGamma signal
 #base_dir = "/ceph/submit/data/group/cms/store/user/mariadlf/D07/GluGluHtoKStar0G_Par-M-125_TuneCP5_13p6TeV_powheg-pythia8-evtgen+RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v2+MINIAODSIM"
 
+#HDstGamma signal
+base_dir = "/ceph/submit/data/group/cms/store/user/mariadlf/D07/GluGluHtoDStar0G_Par-M-125_TuneCP5_13p6TeV_powheg-pythia8-evtgen+RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v2+MINIAODSIM"
+
 #ZPhiGamma signal
 #base_dir = "/ceph/submit/data/group/cms/store/user/mariadlf/D07/ZtoPhiG_TuneCP5_13p6TeV_madgraphMLM-pythia8+RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v3+MINIAODSIM"
 
@@ -76,7 +79,7 @@ files = sorted(files)
 #base_dir = "/ceph/submit/data/group/cms/store/user/mariadlf/D07/ZtoRhoG_TuneCP5_13p6TeV_madgraphMLM-pythia8+RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v3+MINIAODSIM"
 
 
-#files = sorted(glob.glob(f"{base_dir}/*.root"))
+files = sorted(glob.glob(f"{base_dir}/*.root"))
 
 
 
